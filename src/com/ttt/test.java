d53390f9cf479faa6be9e454723655bb2f4fe2b7
YzCatOvO <1315250388@qq.com>
2018-12-05 15:49:36 +0800

    just for test

--- src/com/ttt/test.java
+++ src/com/ttt/test.java
@@ -1,4 +1,8 @@
 package com.ttt;
 
 public class test {
+    public void p() {
+        System.out.println("Hello world ~!!!!");
+    }
+
 }
--- src/com/yky/Main.java
+++ src/com/yky/Main.java
@@ -1,7 +1,8 @@
 package com.yky;
-
+import com.ttt.*;
 public class Main {
     public static void main(String[] args) {
-
+        test t = new test();
+        t.p();
     }
 }
